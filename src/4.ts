aff7fdd305048df2746b280da3adfe043f8eff85
Yuriy Borys <iam618384@gmail.com>
2025-04-06 12:58:28 +0300

    Added task solution

--- src/4.ts
+++ src/4.ts
@@ -1,7 +1,53 @@
+abstract class House {
+    protected key: Key;
+    protected door: boolean = false;
+    protected tenants: Person[];
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    public abstract openDoor(key: Key): void;
+}
+
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.ceil(Math.random() * 999);
+    }
+
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    public getKey(): Key {
+        return this.key;
+    }
+}
+
+class MyHouse extends House {
+    constructor(protected key: Key) {
+        super();
+    }
+
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+const house: MyHouse = new MyHouse(key);
+const person: Person = new Person(key);
 
 house.openDoor(person.getKey());
 
